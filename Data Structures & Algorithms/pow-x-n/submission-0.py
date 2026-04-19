bdeba0dc612ee6893c50b3df34d35f2a2244aad7
cswitzer <50593813+cswitzer@users.noreply.github.com>
2026-04-19 16:24:46 -0500

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def _fast_pow(self, x: float, n: int) -> float:
+        """
+        break exponent in half each time for O(log(n)) time
+        So fast_pow(2.00, 8) would be 2 ** 8
+
+        So 2 ** 8 == (2 ** 4) * (2 ** 4)
+        2 ** 4 == (2 ** 2) * (2 ** 2)
+        2 ** 2 == (2 ** 1) * (2 ** 1)
+        2 ** 1 == (2 ** 0) * (2 ** 0) [Base case]
+
+        What about odd numbers?
+        2 ** 5
+        Since 5 is odd, we peel off an exponent like this to make the numbers even
+
+        - 2 * (2 ** 4), which will be expressed recursively as 2 * (2 ** 2) ** 2
+
+        For negatives, remember the identity x ** -n == 1 / x ** n
+        """
+        if n == 0:
+            return 1
+        if n % 2 == 0:
+            return self._fast_pow(x, n // 2) ** 2
+        else:
+            # peel off 1 exponent to compute an even amount
+            return x * self._fast_pow(x, (n - 1) // 2) ** 2
+
+    def myPow(self, x: float, n: int) -> float:
+        """
+        x to the power of n.
+        """
+        if n >= 0:
+            return self._fast_pow(x, n)
+        return 1 / self._fast_pow(x, -n)
